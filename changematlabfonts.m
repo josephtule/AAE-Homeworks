1a79625e9b42825feffd6d6a60ff45adf58d3cfc
Joseph Le <letujoseph@gmail.com>
2021-09-15 23:16:01 -0400

    nice

---
+++ changematlabfonts.m
@@ -0,0 +1,9 @@
+s = settings;
+fontsize = 20;
+font = 'Times New Roman';
+% font = 'Courier New';
+% font = 'Georgia';
+
+% s.matlab.fonts.editor.title.Color.PersonalValue = [0 0 255 1]; 
+% s.matlab.fonts.editor.normal.Size.PersonalValue = 20;
+s.matlab.fonts.editor.normal.Name.PersonalValue = font;
